2387d18a08e6a5e05b8924fbc6192b679a220b66
Ryan Maloney <rypmaloney@gmail.com>
2021-07-03 11:27:27 -0400

    Validation for password confirmation working. Will refactor the style to work the same for all of the fields

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 const email = document.getElementById('email')
 email.addEventListener('focusout', ()=> emailValid())
 
-
+const passCon = document.getElementById('passConfirm')
+passCon.addEventListener('focusout', ()=> checkPassword())
 
 
 function emailValid(){
@@ -23,12 +24,19 @@ Array.from(inputs).forEach(element => {
 function check(e){
     const warner = document.getElementsByClassName(`${e.target.id}Warn`)
     console.log(warner[0])
-    if(e.target.checkValidity()){
+    if(!e.target.checkValidity()){
        warner[0].style.display = 'block'
     } else {
         warner[0].style.display = 'none'
     }
-
-
 }
 */
+
+function checkPassword(){
+    const form = document.getElementById('form');
+    if(form.passConfirm.value !== form.pass.value){
+        document.getElementById('passWarn').textContent ='Please enter the same password.'
+    }else{
+        document.getElementById('passWarn').textContent =''
+    }
+}
