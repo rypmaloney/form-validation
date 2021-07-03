2aa99bb477361bb2407a5359f743cc428378c58d
Ryan Maloney <rypmaloney@gmail.com>
2021-07-02 20:36:54 -0400

    Email validity check in place. Begun sketching out basic check for required inputs.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,34 @@
 const email = document.getElementById('email')
 email.addEventListener('focusout', ()=> emailValid())
 
+
+
+
 function emailValid(){
+    const emailWarn = document.getElementsByClassName('emailWarn')
     if(email.validity.typeMismatch){
-        console.log('eeeeh!')
+       emailWarn[0].style.display ='block'
+    } else {
+       emailWarn[0].style.display ='none'
+    }
+}
+
+
+/*
+const inputs = document.getElementsByTagName('input')
+
+Array.from(inputs).forEach(element => {
+    element.addEventListener('focusout', (e)=> check(e))})
+
+function check(e){
+    const warner = document.getElementsByClassName(`${e.target.id}Warn`)
+    console.log(warner[0])
+    if(e.target.checkValidity()){
+       warner[0].style.display = 'block'
+    } else {
+        warner[0].style.display = 'none'
     }
 
 
 }
-
-function insertWarning(){
-        
-}
+*/
